65383d181f52ba95fda60230379b18a37d579c77
ERIELSON BRITO DA SILVA <silva.erielson@academico.ifrn.edu.br>
2023-10-16 16:02:10 -0300

    vanessa é linda

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('chat',
+    function(Request $request) {
+        # Apenas um exemplo de resposta. Os dados deveriam vir do banco.
+        return response()->json([
+            [
+                'id' => 1,
+                'usuario' => 'alice',
+                'mensagem' => 'Minha primeira publicação.',
+                'criacao' => '2023-10-07T15:30:00',
+            ],
+            [
+                'id' => 2,
+                'usuario' => 'bob',
+                'mensagem' => 'Publicação exemplo.',
+                'criacao' => '2023-10-08T07:30:00',
+            ]
+        ]);
+    }
+);
