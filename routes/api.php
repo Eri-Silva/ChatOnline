248f20832a45ba690b8795f81327bb77ade4776e
ERIELSON BRITO DA SILVA <silva.erielson@academico.ifrn.edu.br>
2023-10-18 14:12:57 -0300

    falta ajeitar as rotas put e delete

--- routes/api.php
+++ routes/api.php
@@ -26,14 +26,118 @@ Route::get('chat',
                 'id' => 1,
                 'usuario' => 'alice',
                 'mensagem' => 'Minha primeira publicação.',
-                'criacao' => '2023-10-07T15:30:00',
+                'enviada' => '2023-10-07T15:30:00',
             ],
             [
                 'id' => 2,
                 'usuario' => 'bob',
                 'mensagem' => 'Publicação exemplo.',
-                'criacao' => '2023-10-08T07:30:00',
+                'enviada' => '2023-10-08T07:30:00',
             ]
         ]);
+    }
+);
+
+Route::post('chat',
+    function(Request $request) {
+        // o certo seria implementar
+        // $request->validate([
+        //     'usuario' => 'required',
+        //     'mensagem' => 'required|max:300',
+        // ]);
+        return response()->json(
+            [
+                'id' => '02',
+                'usuario' => 'ERI',
+                'mensagem' => "IFRN minha casa",
+                'enviada' => '2023-10-08T09:40:00'
+            ],
+            201
+        );
+    }
+);
+
+Route::get('chat/{id}',
+    function(Request $request, $id) {
+        if ($id == 1) {
+            return response()->json([
+                'id' => 1,
+                'usuario' => 'alice',
+                'mensagem' => 'Minha primeira publicação.',
+                'enviada' => '2023-10-07T15:30:00',
+            ]);
+        }
+
+        if ($id == 2) {
+            return response()->json([
+                'id' => 2,
+                'usuario' => 'bob',
+                'mensagem' => 'Publicação exemplo.',
+                'enviada' => '2023-10-08T07:30:00',
+            ]);
+        }
+
+        return response()->json(
+            [
+                'id' => 1,
+                'usuario' => 'alice',
+                'mensagem' => 'Minha primeira publicação.',
+                'enviada' => '2023-10-07T15:30:00',
+            ],
+            [
+                'id' => 2,
+                'usuario' => 'bob',
+                'mensagem' => 'Publicação exemplo.',
+                'enviada' => '2023-10-08T07:30:00',
+            ],
+            404
+        );
+    }
+);
+
+
+Route::put('chat/{id}',
+    function(Request $request, $id) {
+        $request->validate(['texto' => 'max:144']);
+        # Apenas um exemplo de resposta. Os dados deveriam vir do banco.
+        if (in_array($id, [1,2])) {
+            return response()->json(
+                [
+                    'tipo' => 'info',
+                    'conteudo' => "Publicação alterada (mentira, falta implementar isso).",
+                ]
+            );
+        }
+
+        return response()->json(
+            [
+                'tipo' => 'erro',
+                'conteudo' => 'Não encontrado.'
+            ], 404
+        );
+    }
+);
+
+
+
+Route::delete('chat/{id}',
+    function(Request $request, $id) {
+        # Apenas um exemplo de resposta. Os dados deveriam vir do banco.
+        if (in_array($id, [1,2])) {
+            return response()->json(
+                [
+                    'tipo' => 'info',
+                    'conteudo' => "Publicação apagada (mentira, falta implementar isso).",
+                ]
+            );
+        }
+
+        return response()->json(
+            [
+                'tipo' => 'erro',
+                'conteudo' => 'Publicação não encontrada.'
+            ],
+            404
+        );
     }
 );
